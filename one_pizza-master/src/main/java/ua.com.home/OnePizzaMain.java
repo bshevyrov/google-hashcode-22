b8137a9de77c7b47d276a583b62dd3645846255b
bohdan <bshevyrov@gmail.com>
2022-02-22 19:47:20 +0200

    two layer sort not finish

--- one_pizza-master/src/main/java/ua.com.home/OnePizzaMain.java
+++ one_pizza-master/src/main/java/ua.com.home/OnePizzaMain.java
@@ -11,17 +11,139 @@ import java.nio.file.StandardOpenOption;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class OnePizzaMain {
-   static Map<String,Integer> allLike = new HashMap<>();
-  static   Map<String,Integer> allDislike = new HashMap<>();
+    static Set<String> likeProducts = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
-    private static void sort(Set<Client> clientSet) {
-        Set<Client> tempClientSet = new HashSet<>();
-        Set<Client> likeOneOpinionOneSet = new HashSet<>();
-        Map<String, Set<Client>> likeOneOpinionOneClient = sortLikeOneOpinionOne(clientSet);
-        Map<String, Set<Client>> hhh = sortLikeOneOpinionOne(clientSet);
+    static Set<String> dislikeProducts = Collections.newSetFromMap(new ConcurrentHashMap<>());
+    static Map<String, Integer> allDislike = new HashMap<>();
+    static Map<String, Integer> allLike = new HashMap<>();
+
+
+    /*
+        private static List<Ingredient> findDislikeRatingList(List<Client>clients){
+
+
+            allDislike =
+                    allDislike.entrySet().stream()
+                            .sorted(Map.Entry.comparingByValue()).collect(
+                                    Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
+                                            LinkedHashMap::new));
+        }
+        private static List<Ingredient> findLikeRatingList(List<Client>clients){
+            allLike =
+                    allLike.entrySet().stream()
+                            .sorted(Map.Entry.comparingByValue()).collect(
+                                    Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
+                                            LinkedHashMap::new));
+        }*/
+
+
+    private static List<Client> addClientListToAllIfProductOk(List<Client> all, Client client) {
+
+
+        for (String dislike : client.getDislikes()) {
+            boolean dislikeSomeFromLikeProductList = false;
+            for (String s : likeProducts) {
+                //Сравниванием дислайки нового клиента со списком уже имежщихся лайк продуктов
+                //Если Новый клиент не любит чтото уже из существующего - скипаем клиента
+                //Тут ПОИГРАТЬСЯ С Сортировкой
+                if (StringUtils.equals(dislike, s)) {
+                    dislikeSomeFromLikeProductList = true;
+                    break;
+                }
+            }
+            if (dislikeSomeFromLikeProductList) {
+                break;
+                //
+
+            } else {
+                Set<String> currentClientLikes = client.getLikes();
+                Set<String> currentClientDislikes = client.getDislikes();
+                boolean skip = false;
+
+                //   - каждый новый клиент не должен не любить чтото из списка уже лайкнутых продуктов
+
+                for (String currentClientDislike : currentClientDislikes) {
+                    if (likeProducts.contains(currentClientDislike)) {
+                        skip = true;
+                        break;
+                    }
+                }
+                if (!skip) {
+                    //   - каждый новый клиент не должен любить чтото из списка не любимых продуктов
+
+                    for (String currentClientLike : currentClientLikes) {
+                        if (dislikeProducts.contains(currentClientLike)) {
+                            skip = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!skip) {
+                    likeProducts.addAll(client.getLikes());
+                    dislikeProducts.addAll(client.getDislikes());
+                    all.add(client);
+
+                }
+
+            }
+
+
+        }
+        return all;
+
+    }
+
+
+    private static int findClientsMaxDislikeSize(List<Client> clients) {
+        int tmp = 0;
+        for (Client client : clients) {
+            if (client.getDislikesSize() > tmp) {
+                tmp = (int) client.getDislikesSize();
+            }
+        }
+        return tmp;
+    }
+
+    private static int findClientsMaxLkeSize(List<Client> clients) {
+        int tmp = 0;
+        for (Client client : clients) {
+            if (client.getLikesSize() > tmp) {
+                tmp = (int) client.getLikesSize();
+            }
+        }
+        return tmp;
+    }
+
+    private static int findHowManyPeopleDislikeIngredient(String ingredient) {
+        return
+                null == allDislike.get(ingredient) ? 0 : allDislike.get(ingredient);
+
+    }
+
+    private static List<Client> findClientsWhoLikeByProduct(String product, List<Client> clients) {
+        List<Client> clientsWhoLike = new ArrayList<>();
+        for (Client client : clients) {
+            if (client.getLikes().contains(product)) {
+                clientsWhoLike.add(client);
+            }
+        }
+        return clientsWhoLike;
+    }
+
+    private static List<Client> findClientWhoDislikeByProduct(String product, List<Client> clients) {
+        List<Client> clientsWhoDislike = new ArrayList<>();
+        for (Client client : clients) {
+            if (client.getDislikes().contains(product)) {
+                clientsWhoDislike.add(client);
+            }
+        }
+        return clientsWhoDislike;
+    }
+
+    private static void lessFrequentSort(Set<Client> clientSet) {
 
 
     }
@@ -68,87 +190,75 @@ public class OnePizzaMain {
 
         int count = 0;
 //        Collections.sort(clients, Client.Comparators.EFFICIENT.reversed());
-        clients.sort(Client.Comparators.OP_SIZE);
 
-        System.out.println(Arrays.toString(clients.toArray()));
 
         System.out.println(allLike.toString());
         System.out.println("~~~~");
         System.out.println(allDislike.toString());
 
 
-        Set<String> likeProducts = Collections.newSetFromMap(new ConcurrentHashMap<>());
-        Set<String> dislikeProducts = Collections.newSetFromMap(new ConcurrentHashMap<>());
-        List<Client> removeList = new ArrayList<>();
+        List<Client> finalClients = getOnlyClientsWhiteoutDislikeRating(clients);
+        clients.removeAll(finalClients);
+
+        clients.sort(Client.Comparators.MAX_DIS_MIN_LIKE);
+        System.out.println(clients.toString());
+
+        finalClients.addAll(getOnlYOneLikeAndAllDislikeWithOutDislikeRaiting(clients));
+        clients.removeAll(finalClients);
+
+
+
+
+
+
+        System.out.println("Total clients: " + finalClients.size());
+        System.out.println("likeProducts size= " + likeProducts.size());
+        return likeProducts;
+    }
+
+    private static Collection<? extends Client> getOnlYOneLikeAndAllDislikeWithOutDislikeRaiting(List<Client> clients) {
+        List<Client> rsl = new ArrayList<>();
+        int lkeSize = findClientsMaxLkeSize(clients);
+        int dislikeSize = findClientsMaxDislikeSize(clients);
+        for (int j = 0; j < dislikeSize; j++) {
+
+            for (int i = 0; i < lkeSize; i++) {
+                for (Client client : clients) {
+
+                    if (client.getLikesSize() == lkeSize - i && client.getLikesSize() == dislikeSize) {
+                        boolean noOne = true;
+
+                        for (int k = 0; k < client.getLikesSize(); k++) {
+                            if (findHowManyPeopleDislikeIngredient(client.getLikes().stream().toList().get(k)) > 0){
+                                noOne = false;
+                            }
+                        }
+                        if (noOne) {
+                            addClientListToAllIfProductOk(rsl, client);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(rsl.toString());
+        System.out.println(rsl.size());
+        return rsl;
+    }
+
+    private static List<Client> getOnlyClientsWhiteoutDislikeRating(List<Client> clients) {
+        int count = 0;
+        List<Client> goodClients = new ArrayList<>();
         for (Client client : clients) {
             //if no dislike add product to product set
-            if (client.getDislikesSize() == 0) {
+            if (client.getDislikesSize() == 0 && client.getLikesSize() == 1 && findHowManyPeopleDislikeIngredient(client.getLikes().stream().findFirst().get()) == 0) {
                 likeProducts.addAll(client.getLikes());
-                removeList.add(client);
+                goodClients.add(client);
                 count++;
             }
 
         }
-        //del clients with zero dislikes frome clientsList
-        for (Client client : removeList) {
-            clients.remove(client);
-        }
-
-        for (Client client : clients) {
-
-            for (String dislike : client.getDislikes()) {
-                boolean dislikeSomeFromLikeProductList = false;
-                for (String s : likeProducts) {
-                    //Сравниванием дислайки нового клиента со списком уже имежщихся лайк продуктов
-                    //Если Новый клиент не любит чтото уже из существующего - скипаем клиента
-                    //Тут ПОИГРАТЬСЯ С Сортировкой
-                    if (StringUtils.equals(dislike, s)) {
-                        dislikeSomeFromLikeProductList = true;
-                        break;
-                    }
-                }
-                if (dislikeSomeFromLikeProductList) {
-                    break;
-                    //
-
-                } else {
-                    Set<String> currentClientLikes = client.getLikes();
-                    Set<String> currentClientDislikes = client.getDislikes();
-                    boolean skip = false;
-
-                    //   - каждый новый клиент не должен не любить чтото из списка уже лайкнутых продуктов
-
-                    for (String currentClientDislike : currentClientDislikes) {
-                        if (likeProducts.contains(currentClientDislike)) {
-                            skip = true;
-                            break;
-                        }
-                    }
-                    if (!skip) {
-                        //   - каждый новый клиент не должен любить чтото из списка не любимых продуктов
-
-                        for (String currentClientLike : currentClientLikes) {
-                            if (dislikeProducts.contains(currentClientLike)) {
-                                skip = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (!skip) {
-                        count++;
-                        likeProducts.addAll(client.getLikes());
-                        dislikeProducts.addAll(client.getDislikes());
-                    }
-
-                }
-            }
-
-        }
-
-        System.out.println("Total clients: " + count);
-        System.out.println("likeProducts size= " + likeProducts.size());
-        return likeProducts;
+        System.out.println("getOnlyOneLikesClients: " + count);
+        return goodClients;
     }
 
 
@@ -188,32 +298,36 @@ public class OnePizzaMain {
 //            System.out.println("L:"+client.getLikesSize()+" D:" + client.getDislikesSize() +
 //                    " E:" + client.getEfficient() + " O:" + client.getOpinionSize());
         }
-        allLike =
-                 allLike.entrySet().stream()
-                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(
-                                Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
-                                 LinkedHashMap::new));
+
+
         allDislike =
                 allDislike.entrySet().stream()
-                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(
+                        .sorted(Map.Entry.comparingByValue()).collect(
                                 Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
                                         LinkedHashMap::new));
+        allLike =
+                allLike.entrySet().stream()
+                        .sorted(Map.Entry.comparingByValue()).collect(
+                                Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
+                                        LinkedHashMap::new));
+
+
         return clients;
     }
 
     private static void addToDislikeStatistic(String s) {
-        if(allDislike.containsKey(s)){
-            allDislike.put(s, allDislike.get(s) +1);
-         } else {
-            allDislike.put(s,1);
-    }
+        if (allDislike.containsKey(s)) {
+            allDislike.put(s, allDislike.get(s) + 1);
+        } else {
+            allDislike.put(s, 1);
+        }
     }
 
     private static void addToLikeStatistic(String s) {
-        if(allLike.containsKey(s)){
-            allLike.put(s,allLike.get(s).intValue()+1);
+        if (allLike.containsKey(s)) {
+            allLike.put(s, allLike.get(s).intValue() + 1);
         } else {
-            allLike.put(s,1);
+            allLike.put(s, 1);
         }
     }
 
@@ -289,6 +403,7 @@ class Client implements Comparable<Client> {
     private double efficient;
     private double opinionSize;
 
+
     public double getOpinionSize() {
         return opinionSize;
     }
@@ -348,8 +463,11 @@ class Client implements Comparable<Client> {
 
     public static class Comparators {
         public static final Comparator<Client> OP_SIZE = Comparator.comparingDouble(Client::getOpinionSize);
+        public static final Comparator<Client> MAX_DIS = Comparator.comparingDouble(Client::getDislikesSize).reversed();
+        public static final Comparator<Client> MIN_LIKE = Comparator.comparingDouble(Client::getLikesSize);
         public static final Comparator<Client> EFFICIENT = Comparator.comparingDouble(Client::getEfficient);
         public static final Comparator<Client> OP_AND_EFF = (Client o1, Client o2) -> EFFICIENT.thenComparing(OP_SIZE).compare(o1, o2);
+        public static final Comparator<Client> MAX_DIS_MIN_LIKE = (Client o1, Client o2) -> MAX_DIS.thenComparing(MIN_LIKE).compare(o1, o2);
     }
 
     @Override
@@ -360,5 +478,36 @@ class Client implements Comparable<Client> {
                 " E:" + efficient +
                 " O:" + opinionSize +
                 '}' + "\n";
+    }
+}
+
+class Ingredient {
+
+    private String name;
+    private int allLikes;
+    private int allDislikes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAllLikes() {
+        return allLikes;
+    }
+
+    public void setAllLikes(int allLikes) {
+        this.allLikes = allLikes;
+    }
+
+    public int getAllDislikes() {
+        return allDislikes;
+    }
+
+    public void setAllDislikes(int allDislikes) {
+        this.allDislikes = allDislikes;
     }
 }
